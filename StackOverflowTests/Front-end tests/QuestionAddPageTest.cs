bb3f159a4e6f30fbddaf124bd8348093c1c7750b
Александр Петроченко <e44alex@gmail.com>
2020-11-11 17:43:56 +0300

    Started tests for front-end

---
+++ StackOverflowTests/Front-end tests/QuestionAddPageTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using ApiFrontEnd.Pages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moq;
+using NUnit.Framework;
+using StackOverflowWebApi.Models;
+using StackOverflowWebApi.Services;
+
+namespace StackOverflowTests
+{
+    public class QuestionAddPageTest
+    {
+        [Ignore("HttpContext used")]
+        [Test]
+        public async Task QuestionCreatePage_OnPost_Works()
+        {
+            var apiClientMock = new Mock<IApiClient>();
+            apiClientMock
+                .Setup(x => x.AddQuestionAsync(new Question(), "token"))
+                .ReturnsAsync(true);
+
+            apiClientMock
+                .Setup(x => x.GetUserIdAsync("Username"))
+                .ReturnsAsync(Guid.NewGuid());
+            
+
+            var subjectUnderTest = new QuestionCreateModel(apiClientMock.Object);
+            subjectUnderTest.PageContext = new PageContext();
+            subjectUnderTest.PageContext.HttpContext = new DefaultHttpContext();
+            subjectUnderTest.PageContext.HttpContext.Response.Cookies.Append("token", "token");
+            subjectUnderTest.PageContext.HttpContext.Response.Cookies.Append("user", "token");
+            var question = new Question();
+            var result = await subjectUnderTest.OnPostAsync(question);
+
+            Assert.AreNotEqual(question.Id, Guid.Empty);
+            Assert.IsNotNull(question.Creator);
+            Assert.AreNotEqual(question.Creator.Id, Guid.Empty);
+            Assert.IsInstanceOf<RedirectResult>(result);
+        }
+    }
+}
